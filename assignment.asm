90ed7832e37222713f72790f7c49a51339ae3a5c
hungHCMUT <hung.nguyen2205@hcmut.edu.vn>
2025-04-30 22:27:19 +0700

    Adding showUpdatedBoard, Edit logic of promptCoord, adding Update Board

--- assignment.asm
+++ assignment.asm
@@ -2,10 +2,10 @@
 	#shared: board, rules prompt, continue prompt, invalid messages
 	#each player: prompt to get coordinate
 	board: .space 225        # 15 x 15 = 225 cells
-    	displayBoard: .space 1350 # each cell display 6 bytes, 225 * 6 = 1350
-    	str_endl: .asciiz "\n"
-    	str_space: .asciiz "   "
-    	str_header: .asciiz "     _0_   _1_   _2_   _3_   _4_   _5_   _6_   _7_   _8_   _9_   _10   _11   _12   _13   _14\n"
+	displayBoard: .space 2000
+	str_endl: .asciiz "\n"
+	str_space: .asciiz "   "
+	str_header: .asciiz "     _0_   _1_   _2_   _3_   _4_   _5_   _6_   _7_   _8_   _9_   _10   _11   _12   _13   _14\n"
 	rules: .asciiz "abc"
 	coordPrompt1: .asciiz "Player 1, please input your coordinates: "
 	coordPrompt2: .asciiz "Player 2, please input your coordinates: "
@@ -32,8 +32,9 @@
 				# execute
 	jal makeBoard
 gamePlay:
-	jal showBoard
-	#jal promptCoord
+	li $s0, 0 # WARNING : This is global variable, so it will be 0 for player 1 and 1 for player 2
+	jal showInitBoard
+	jal promptCoord
 # 	#jal showBoard
 # 	#jal checkWinner #already printed winner message, return in $v0 1 if game ends, else 0
 # 	#beqz gamePlay
@@ -118,7 +119,7 @@ makeBoard:
  	addi $sp, $sp, 4
  	jr $ra
  	
-showBoard:
+showInitBoard:
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
 
@@ -205,67 +206,244 @@ print_cell_char:
 	addi $sp, $sp, 4
 	jr $ra
 
-# promptCoord:
-# # ---------------------------------------------------------------------------------------------------- #
-# #   	Arguments:
-# #	$s0 contains flag: 0 - player 1 ; 1 - player 2
-# # ---------------------------------------------------------------------------------------------------- #
-# 	addi $sp, $sp, -4
-# 	sw $ra, 0($sp)
-# 	jal stackIn
-# 	beqz handlePlayer2
-# 	handlePlayer1:
-# 		#prompt
-# 		jal validateCoord #out: $v0: 0 --> invalid, 1 --> valid; $s2: row; $s3: col . Please print msg there
-# 		beqz handlePlayer1
-# 		j promptCoord_end
-# 	handlePlayer2:
-# 		#prompt
-# 		jal validateCoord
-# 		beqz handlePlayer2
-# promptCoord_end:
-# 	jal updateBoard #in: $s2: row; $s3: col
-# 	jal stackOut
-#  	lw $ra, 0($sp)
-#  	addi $sp, $sp, 4
-#  	jr $ra
-	
-# validateCoord:
-# # ---------------------------------------------------------------------------------------------------- #
-# #	Return:
-# #	$v0: 0 if invalid else 1
-# #	$s2: row
-# #	$s3: col
-# # ---------------------------------------------------------------------------------------------------- #
-# 	addi $sp, $sp, -4
-# 	sw $ra, 0($sp)
-# 	jal stackIn
-	
-# 	#TODO
-	
-# 	jal stackOut
-#  	lw $ra, 0($sp)
-#  	addi $sp, $sp, 4
-#  	jr $ra
+showUpdatedBoard:
+# ---------------------------------------------------------------------------------------------------- #
+#   Function to display the updated board after a move
+#   Uses the 'board' array to update the 'displayBoard' and then displays it
+#   board: Contains 0 for empty, 1 for player 1 (X), 2 for player 2 (O)
+#   displayBoard: Visual representation with "___" for empty, "_X_" for player 1, "_O_" for player 2
+# ---------------------------------------------------------------------------------------------------- #
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
 
-# updateBoard:
-# # ---------------------------------------------------------------------------------------------------- #
-# #	Arguments:
-# #	$s0: 0 if player 1 else player 2
-# #	$s2: row
-# #	$s3: col
-# #	Description: update both 'board' and 'displayBoard' 
-# # ---------------------------------------------------------------------------------------------------- #
-# 	addi $sp, $sp, -4
-# 	sw $ra, 0($sp)
-# 	jal stackIn
+	# First, update displayBoard based on current board values
+	la $t0, board         # $t0 = address of board
+	la $t1, displayBoard  # $t1 = address of displayBoard
+	li $t2, 0             # cell counter
+
+update_display_loop:
+	lb $t3, 0($t0)        # Load board value (0, 1, or 2)
 	
-# 	#TODO
+	# Each displayBoard cell has 6 characters: "___   " or "_X_   " or "_O_   "
+	li $t5, 6
+	mul $t4, $t2, $t5     # Calculate offset in displayBoard
+	add $t4, $t1, $t4     # Address of current displayBoard cell
 	
-# 	jal stackOut
-#  	lw $ra, 0($sp)
-#  	addi $sp, $sp, 4
-#  	jr $ra
+	# First and third character remain '_' for all cells
+	li $t5, 95            # ASCII '_'
+	sb $t5, 0($t4)        # First '_'
+	sb $t5, 2($t4)        # Third '_'
+	
+	# Middle character depends on board value
+	beqz $t3, empty_cell  # If board value is 0, cell is empty
+	li $t6, 1
+	beq $t3, $t6, player1_cell # If board value is 1, cell is player 1 (X)
+	
+	# Otherwise, it's player 2 (O)
+	li $t5, 79            # ASCII 'O'
+	sb $t5, 1($t4)
+	j cell_marked
+	
+player1_cell:
+	li $t5, 88            # ASCII 'X'
+	sb $t5, 1($t4)
+	j cell_marked
+	
+empty_cell:
+	li $t5, 95            # ASCII '_'
+	sb $t5, 1($t4)
+	
+cell_marked:
+	# Last 3 characters are spaces
+	li $t5, 32            # ASCII space
+	sb $t5, 3($t4)
+	sb $t5, 4($t4)
+	sb $t5, 5($t4)
+	
+	addi $t0, $t0, 1      # Move to next board cell
+	addi $t2, $t2, 1      # Increment counter
+	li $t3, 225           # 15x15 board has 225 cells
+	blt $t2, $t3, update_display_loop
+
+	# Now display the updated board
+	li $v0, 4
+	la $a0, str_header
+	syscall
+
+	la $t0, displayBoard
+	li $t1, 0             # row counter
+
+print_updated_row:
+	# Print row number
+	li $v0, 1
+	move $a0, $t1
+	syscall
+
+	# Print spacing after row number
+	li $v0, 4
+	la $a0, str_space
+	syscall
+	
+	# Add extra space for single-digit row numbers (0-9)
+	li $t6, 10
+	bge $t1, $t6, skip_updated_extra_space  # If row >= 10, skip extra space
+	li $v0, 11
+	li $a0, 32                      # ASCII space
+	syscall
+
+skip_updated_extra_space:
+
+	li $t2, 0             # col counter
+print_updated_col:
+	li $t4, 0             # char index within each cell
+print_updated_cell_char:
+	lb $a0, 0($t0)
+	li $v0, 11
+	syscall
+	addi $t0, $t0, 1
+	addi $t4, $t4, 1
+	li $t5, 6
+	blt $t4, $t5, print_updated_cell_char
+
+	addi $t2, $t2, 1
+	li $t3, 15
+	blt $t2, $t3, print_updated_col
+
+	li $v0, 4
+	la $a0, str_endl
+	syscall
+
+	addi $t1, $t1, 1
+	li $t3, 15
+	blt $t1, $t3, print_updated_row
+
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
+
+promptCoord:
+# ---------------------------------------------------------------------------------------------------- #
+#   	Arguments:
+#	$s0 contains flag: 0 - player 1 ; 1 - player 2
+# 	$v0: 0 --> invalid, 1 --> valid; $s2: row; $s3: col 
+# ---------------------------------------------------------------------------------------------------- #
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal stackIn
+	
+	beqz $s0, handlePlayer1
+	j handlePlayer2
+	
+handlePlayer1:
+	# Display prompt for Player 1
+	li $v0, 4
+	la $a0, coordPrompt1
+	syscall
+
+	li $v0, 4
+	la $a0, str_endl
+	syscall
+
+
+	# Hard code coordinates for Player 1
+	li $s2, 5            # Row 5 
+	li $s3, 5            # Column 5
+	
+	# Here you would normally validate the coordinates
+	# jal validateCoord  # out: $v0: 0 --> invalid, 1 --> valid; $s2: row; $s3: col
+	# beqz $v0, handlePlayer1  # If invalid, prompt again
+	
+	jal updateBoard      # in: $s2: row; $s3: col
+	jal showUpdatedBoard # Display the updated board
+	
+	# Toggle player for next turn
+	li $s0, 1
+	
+handlePlayer2:
+	# Display prompt for Player 2
+	li $v0, 4
+	la $a0, coordPrompt2
+	syscall
+
+	li $v0, 4
+	la $a0, str_endl
+	syscall
+	
+	# Hard code coordinates for Player 2
+	li $s2, 6            # Row 6
+	li $s3, 6            # Column 6
+	
+	# Here you would normally validate the coordinates
+	# jal validateCoord  # out: $v0: 0 --> invalid, 1 --> valid; $s2: row; $s3: col
+	# beqz $v0, handlePlayer2  # If invalid, prompt again
+	
+	jal updateBoard      # in: $s2: row; $s3: col
+	jal showUpdatedBoard # Display the updated board
+	
+	# Toggle player for next turn
+	li $s0, 0
+		
+promptCoord_end:
+	jal stackOut
+ 	lw $ra, 0($sp)
+ 	addi $sp, $sp, 4
+ 	jr $ra
+	
+validateCoord:
+# ---------------------------------------------------------------------------------------------------- #
+#	Return:
+#	$v0: 0 if invalid else 1
+#	$s2: row
+#	$s3: col
+# ---------------------------------------------------------------------------------------------------- #
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal stackIn
+	
+	#TODO
+	
+	jal stackOut
+ 	lw $ra, 0($sp)
+ 	addi $sp, $sp, 4
+ 	jr $ra
+
+updateBoard:
+# ---------------------------------------------------------------------------------------------------- #
+#	Arguments:
+#	$s0: 0 if player 1 else player 2
+#	$s2: row
+#	$s3: col
+#	Description: update both 'board' and 'displayBoard' 
+# ---------------------------------------------------------------------------------------------------- #
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal stackIn
+	
+	# Calculate the index in the board array (row_major: index = row * cols + col)
+	li $t0, 15           # Number of columns in the board
+	mul $t1, $s2, $t0    # row * cols
+	add $t1, $t1, $s3    # row * cols + col = index in board array
+	
+	# Get the base address of the board array
+	la $t2, board
+	add $t2, $t2, $t1    # Actual memory address to update
+	
+	# Determine which value to store based on current player
+	beqz $s0, player1_mark
+	li $t3, 2            # Player 2 (O) = 2
+	j store_mark
+	
+player1_mark:
+	li $t3, 1            # Player 1 (X) = 1
+	
+store_mark:
+	# Store the player's mark at the calculated position
+	sb $t3, 0($t2)       # Update the board array with player's mark
+	
+	jal stackOut
+ 	lw $ra, 0($sp)
+ 	addi $sp, $sp, 4
+ 	jr $ra
 
 # checkWinner:
 # # ---------------------------------------------------------------------------------------------------- #
@@ -302,31 +480,31 @@ print_cell_char:
 #  	addi $sp, $sp, 4
 #  	jr $ra
 																																						
-# stackIn:
-# # ---------------------------------------------------------------------------------------------------- #
-# # Should use this to avoid wrong value of registers when call functions, NOTE that $ra is not included
-# # ---------------------------------------------------------------------------------------------------- #
-# 	addi $sp, $sp, -32
-# 	sw $s0, 0($sp)
-# 	sw $s1, 4($sp)
-# 	sw $t0, 8($sp)
-# 	sw $t1, 12($sp)
-# 	sw $t2, 16($sp)
-# 	sw $t3, 20($sp)
-# 	sw $t4, 24($sp)
-# 	sw $t5, 28($sp)
-# 	jr $ra
-# stackOut:
-# 	lw $s0, 0($sp)
-# 	lw $s1, 4($sp)
-# 	lw $t0, 8($sp)
-# 	lw $t1, 12($sp)
-# 	lw $t2, 16($sp)
-# 	lw $t3, 20($sp)
-# 	sw $t4, 24($sp)
-# 	sw $t5, 28($sp)
-# 	addi $sp, $sp, 32
-# 	jr $ra
+stackIn:
+# ---------------------------------------------------------------------------------------------------- #
+# Should use this to avoid wrong value of registers when call functions, NOTE that $ra is not included
+# ---------------------------------------------------------------------------------------------------- #
+	addi $sp, $sp, -32
+	sw $s0, 0($sp)
+	sw $s1, 4($sp)
+	sw $t0, 8($sp)
+	sw $t1, 12($sp)
+	sw $t2, 16($sp)
+	sw $t3, 20($sp)
+	sw $t4, 24($sp)
+	sw $t5, 28($sp)
+	jr $ra
+stackOut:
+	lw $s0, 0($sp)
+	lw $s1, 4($sp)
+	lw $t0, 8($sp)
+	lw $t1, 12($sp)
+	lw $t2, 16($sp)
+	lw $t3, 20($sp)
+	sw $t4, 24($sp)
+	sw $t5, 28($sp)
+	addi $sp, $sp, 32
+	jr $ra
  	
 
 	
